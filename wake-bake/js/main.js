38e59235506157b13f252ebe78028d0b23b148ff
CozyDozy <112979000+CozyDozy@users.noreply.github.com>
2025-11-13 21:38:02 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -180,6 +180,15 @@ breakpoints: {
 
 });
 
+    //Маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
+
+
+
+
 
     }
 )()
