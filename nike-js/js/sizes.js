0fd98794b6f456e55d9bfae16e113c744e94d43e
CozyDozy <112979000+CozyDozy@users.noreply.github.com>
2026-05-30 01:54:56 +0300

    Add files via upload

--- nike-js/js/sizes.js
+++ nike-js/js/sizes.js
@@ -1,4 +1,13 @@
 const productSizesButtons = document.querySelectorAll(".product__sizes-button");
+const x = document.querySelector('.product__sizes-button--active')
+
+
+const local = localStorage.getItem('activeButton');
+if (local) {
+    productSizesButtons.forEach(btn => {
+        btn.classList.toggle('product__sizes-button--active', btn.textContent.trim() === local);
+    });
+}
 
 
 productSizesButtons.forEach(button => {
@@ -9,6 +18,8 @@ productSizesButtons.forEach(button => {
     })
 
     button.classList.add("product__sizes-button--active")
+
+    localStorage.setItem('activeButton', button.textContent.trim())
   })
 })
 
